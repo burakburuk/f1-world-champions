2441d58d86c6d2436d0943426c239b922dccbeac
Burak Buruk <burak.bburuk@gmail.com>
2019-10-16 21:55:29 +0200

    linting errors fixed

--- application/src/reducers/notificationReducer.spec.js
+++ application/src/reducers/notificationReducer.spec.js
@@ -1,25 +1,24 @@
 import notificationReducer from './notificationReducer';
 import initialState from './initialState';
-import {showNotificationBox, closeNotificationBox} from '../actions';
+import { showNotificationBox, closeNotificationBox } from '../actions';
 
-describe('Given a notificiation reduver', () => {
-
+describe('Given notificiation reducer', () => {
     describe('when reducer called without state', () => {
         it('should return initial state', () => {
             const actual = notificationReducer(undefined, undefined);
             const expected = initialState.notification;
             expect(actual).toEqual(expected);
-        })
+        });
     });
 
     describe('when reducer called with show notification action', () => {
         it('should return state with test notification', () => {
-            const notification = {title: 'Test', message: 'Test Message'};
+            const notification = { title: 'Test', message: 'Test Message' };
             const action = showNotificationBox(notification);
             const actual = notificationReducer(undefined, action);
             expect(actual.title).toEqual(notification.title);
             expect(actual.message).toEqual(notification.message);
-        })
+        });
     });
 
     describe('when reducer called with close notification action', () => {
@@ -28,7 +27,6 @@ describe('Given a notificiation reduver', () => {
             const actual = notificationReducer(undefined, action);
             const expected = initialState.notification;
             expect(actual).toEqual(expected);
-        })
+        });
     });
-
 });
